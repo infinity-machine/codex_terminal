b89fa131e204f9b1aae638bd9ef17bf72264a332
Connor Carciofini <ccarciofini@gmail.com>
2023-01-09 21:16:17 -0600

    working

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 const inquirer = require('inquirer');
-
+const fetch = require('node-fetch');
+require('dotenv').config()
 
 const prompt = [{
     type: 'input',
-    name: 'q',
+    name: 'query',
     message: 'q'
 }];
 
 
-function runPrompt(){
-    return inquirer.prompt(prompt)
-    .then(()=> {
-        console.log('HERE IS WHERE THE ANSWER WILL GO')
-        runPrompt();
+async function runPrompt(){
+    console.log('NOW CHATTING WITH CODEX')
+    const resolve = await inquirer.prompt(prompt)
+    const response = await fetch('http:localhost:6969', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            // 'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+        },
+        body: JSON.stringify(resolve)
     });
-}
+    if (response.ok) {
+        const data = await response.json();
+        const parsed_data = data.bot.trim();
+        console.log(parsed_data)
+    }
+};
 
 runPrompt()
